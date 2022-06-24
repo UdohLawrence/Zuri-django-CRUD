f728d2aae2a5723df0e55eb9854816a9273d5d72
UdohLawrence <lawrenceaudoh@gmail.com>
2022-06-24 21:18:38 +0100

    fixing errors

--- I4G003420FWQ/blog/views.py
+++ I4G003420FWQ/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import DetailView
@@ -15,18 +16,18 @@ class PostListView(ListView):
 
 class PostCreateView(CreateView):
   model = Post
-  fields = “__all__”
-  success_url = reverse_lazy(“blog: all”)
+  fields = '__all__'
+  success_url = reverse_lazy('blog: all')
 
 class PostDetailView(DetailView):
   model = Post
   
 class PostUpdateView(UpdateView):
   model = Post
-  fields = “__all__”
-  success_url  = reverse_lazy(“blog:all”)
+  fields = '__all__'
+  success_url  = reverse_lazy('blog:all')
 
 class PostDeleteView(UpdateView):
   model = Post
-  fields = “__all__”
-  success_url  = reverse_lazy(“blog:all”)
+  fields = '__all__'
+  success_url  = reverse_lazy('blog:all')
