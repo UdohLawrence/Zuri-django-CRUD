6ccf2fda558161dfd4eea650f3e687cadc29095b
UdohLawrence <lawrenceaudoh@gmail.com>
2022-06-24 21:27:11 +0100

    Errors fixing

--- I4G003420FWQ/blog/views.py
+++ I4G003420FWQ/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
-from django.views.generic.edit import DetailView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView
 
 
